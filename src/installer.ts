e6df956843305671b41548bbea1bd176f5822c1a
Jeremy Shore <xboxmusicn3rd@gmail.com>
2022-11-14 22:54:54 -0800

    Don't use npm to install

--- src/installer.ts
+++ src/installer.ts
@@ -1,16 +1,17 @@
-import { getInput, startGroup, endGroup } from '@actions/core';
+import { getInput, debug, startGroup, endGroup } from '@actions/core';
 import { exec } from '@actions/exec';
 
 export const install = async () => {
   startGroup('Firebase-Tools Installer');
   const version = getInput('tools-version');
+
   let command = 'npm install -g firebase-tools';
 
   if (version) {
-    
+    debug(`Using version ${version}`);
     command = `${command}@${version}`;
   }
 
-  await exec(command);
+  await exec('curl -sL firebase.tools | bash');
   endGroup();
 }
